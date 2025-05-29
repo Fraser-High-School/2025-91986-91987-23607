b5739f2d32a5a511f1f9ac9c21c3c4f76a698c9c
23607 <23607@fraser.school.nz>
2025-05-29 13:50:50 +1200

    Began working on supper_effective
    added more type interactions

--- test.py
+++ test.py
@@ -6,12 +6,23 @@ def super_effective(move, target):
     #Fire type interactions
     if move.ptype == 'fire' and target.type1 == 'grass':
         return 2
-    elif move.ptype == 'fire' and target.type1 == 'water':
+    elif move.ptype == 'fire' and target.type1 == 'water' or move.ptype == 'fire' and target.type1 == 'fire':
         return 0.5
     #Dark type interactions
-    elif move.ptype == 'dark' and target.type1 == 'fairy' or move.ptype == 'dark' and target.type2 == 'fighting'
-    elif move.ptype == 'water' and target.type1 ==
-    elif move.ptype == 'grass' and target.type1 ==
+    elif move.ptype == 'dark' and target.type1 == 'fairy' or move.ptype == 'dark' and target.type2 == 'fighting':
+        return 0.5
+    #Water type interactions
+    elif move.ptype == 'water' and target.type1 == 'fire':
+        return 2
+    elif move.ptype == 'water' and target.type1 == 'water' or move.ptype == 'water' and target.type1 == 'grass':
+        return 0.5
+    #Grass type interactions
+    elif move.ptype == 'grass' and target.name == 'swampert':
+        return 4
+    elif move.ptype == 'grass' and target.type1 == 'grass' or target.name == 'togekiss':
+        return 0.5
+    elif move.ptype == 'grass' and target.name == 'charizard':
+        return 0.25
     elif move.ptype == 'fairy' and target.type1 ==
     elif move.ptype == 'flying' and target.type1 ==
     elif move.ptype == 'fire' and target.type1 ==
