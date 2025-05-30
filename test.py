8fc7fc1435921e93002138d07c4b2cfdbc9027fe
23607 <23607@fraser.school.nz>
2025-05-30 14:45:07 +1200

    supper_effective finished and ready for testing

--- test.py
+++ test.py
@@ -25,9 +25,42 @@ def super_effective(move, target):
         return 0.25
     #Fairy type interactions
     elif move.ptype == 'fairy' and target.type1 == 'fairy' or move.ptype == 'fairy' and target.type1 == 'fire':
-        return 
-    elif move.ptype == 'flying' and target.type1 ==
-    elif move.ptype == 'fire' and target.type1 ==
-    elif move.ptype == 'fire' and target.type1 ==
-    elif move.ptype == 'fire' and target.type1 ==
+        return 0.5
+    elif move.ptype == 'fairy' and target.type2 == 'fighting':
+        return 2
+    #Flying type interactions
+    elif move.ptype == 'flying' and target.name == 'decidueye'
+        return 4
+    #ground type interactions
+    elif move.ptype == 'ground' and target.type2 == 'flying':
+        return 0
+    elif move.ptype == 'ground' and target.type1 == 'grass':
+        return 0.5
+    #Steel type interactions
+    elif move.ptype == 'steel' and target.type1 == 'fairy':
+        return 2
+    elif move.ptype == 'steel' and target.type1 == 'fire' or move.ptype == 'steel' and target.type2 == 'fighting' or move.ptype == 'steel' and target.type1 == 'water':
+        return 0.5
+    #Rock type interactions
+    elif move.ptype == 'rock' and target.type1 == 'water':
+        return 0.5
+    elif move.ptype == 'rock' and target.name == 'charizard':
+        return 4
+    elif move.ptype == 'rock' and target.name == 'togekiss':
+        return 2
+    elif move.ptype == 'rock' and target.name == 'decidueye':
+        return 0.25
+    #Fighting type interactions
+    elif move.ptype == 'fighting' and target.type1 == 'fairy' or move.ptype == 'fighting' and target.type1 == 'grass':
+        return 0.5
+    #Ice type interactions
+    elif move.ptype == 'ice' and target.name == 'Togekiss':
+        return 2
+    #electric type interactions
+    elif move.ptype == 'electric' and target.type1 == 'grass':
+        return 0.5
+    elif move.ptype == 'electric' and target.type2 == 'ground':
+        return 0
+    elif move.ptype == 'electric' and target.type2 == 'flying':
+        return 2
 #Test case
