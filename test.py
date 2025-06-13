b76a8580343e8fce43e55ae1c514f307eba2aca9
23607 <23607@fraser.school.nz>
2025-06-13 14:51:22 +1200

    Total rework of super_effective
    in progress

--- test.py
+++ test.py
@@ -3,9 +3,69 @@ from Pokédex import *
 #Functions
 def super_effective(move, target):
     effectiveness = 1
+    #Fire type interactions
     if move.ptype == 'fire' and 'grass' in target.typing:
+        effectiveness = effectiveness * 2
+    if move.ptype == 'fire' and 'water' or 'fire' in target.typing:
+        effectiveness = effectiveness * 0.5
+    #Dark type interactions
+    if move.ptype == 'dark' and 'fairy' or 'fighting' in target.typing:
+        effectiveness = effectiveness * 0.5
+    #Water type interactions
+    if move.ptype == 'water' and 'fire' in target.typing:
+        effectiveness = effectiveness * 2
+    if move.ptype == 'water' and 'water' or 'grass' in target.typing:
+        effectiveness = effectiveness * 0.5
+    #Grass type interactions
+    if move.ptype == 'grass' and 'water' or'ground' in target.typing:
+        effectiveness = effectiveness * 2
+    if move.ptype == 'grass' and 'grass' or 'flying' or 'fire' in target.typing:
+        effectiveness = effectiveness * 0.5
+    #Fairy type interactions
+    if move.ptype == 'fairy' and 'fairy' or 'fire' in target.typing:
+        effectiveness = effectiveness * 0.5
+    if move.ptype == 'fairy' and 'fighting' in target.typing:
+        effectiveness = effectiveness * 2
+    #Flying type interactions
+    if move.ptype == 'flying' and 'grass' or 'fighting' in target.typing:
+        effectiveness = effectiveness * 2
+    #ground type interactions
+    if move.ptype == 'ground' and == 'flying':
+        return 0
+    if move.ptype == 'ground' and 'grass':
+        effectiveness = effectiveness * 0.5
+    #Steel type interactions
+    if move.ptype == 'steel' and 'fairy':
+        effectiveness = effectiveness * 2
+    if move.ptype == 'steel' and 'fire' or move.ptype == 'ste' and == 'fighting' or move.ptype == 'ste' and 'water':
+        effectiveness = effectiveness * 0.5
+    #Rock type interactions
+    if move.ptype == 'rock' and 'water':
+        effectiveness = effectiveness * 0.5
+    if move.ptype == 'rock' and 'fire':
+        effectiveness = effectiveness * 2
+    if move.ptype == 'rock' and == 'flying':
+        effectiveness = effectiveness * 2
+    if move.ptype == 'rock' and == 'fighting':
+        effectiveness = effectiveness * 0.5
+    if move.ptype == 'rock' and 'grass':
+        effectiveness = effectiveness * 0.5
+    #Fighting type interactions
+    if move.ptype == 'fighting' and 'fairy' or move.ptype == 'fighting' and 'grass':
+        effectiveness = effectiveness * 0.5
+    #Ice type interactions
+    if move.ptype == 'ice' and == 'flying':
+        effectiveness = effectiveness * 2
+    if move.ptype == 'ice' and 'fire':
+        effectiveness = effectiveness * 0.5
+    #electric type interactions
+    if move.ptype == 'electric' and 'grass':
+        effectiveness = effectiveness * 0.5
+    if move.ptype == 'electric' and == 'ground':
+        return 0
+    if move.ptype == 'electric' and == 'flying':
         effectiveness = effectiveness * 2
     return effectiveness
 #Main
-a = super_effective(flamethrower, decidueye1)
+a = super_effective(flamethrower, charizard1)
 print(a)
