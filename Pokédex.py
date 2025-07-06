e1612c11513c50e1ebb997caaf14eb8ca7746dcd
23607 <23607@fraser.school.nz>
2025-07-07 02:00:39 +1200

    Added a new move attribute: effect_qty
    Also continued working on move_effect. effect_qty establishes a number to use for multiplications inherent to a move's effect

--- Pokédex.py
+++ Pokédex.py
@@ -51,10 +51,9 @@ class move:
     Other attributes present in the orignal material were disregarded as they
     will not be used for this simulation, like priority, field effects, etc.
     """
-    def __init__(self, move_name, base_power, effect, effect_chance, acc, ptype, crit_rate, category, effect_id):
+    def __init__(self, move_name, base_power, effect_chance, acc, ptype, crit_rate, category, effect_id, effect_qty):
         self.move_name = move_name
         self.base_power = base_power
-        self.effect = effect
         self.acc = acc
         self.ptype = ptype
         self.crit_rate = crit_rate
@@ -63,58 +62,59 @@ class move:
         self.category = category
         self.effect_chance = effect_chance
         self.effect_id = effect_id
+        self.effect_qty = effect_qty
 
 
 #Toolbox Charizard moves:    
-flamethrower = move('flamethrower', 90, False, 0, 100, 'fire', 417, 'special', 'na')
-solar_beam = move('solar beam', 120, True, 100, 100, 'grass', 417, 'special', 'charge')
-air_slash = move('air slash', 75, True, 30, 95, 'flying', 417, 'special', 'na')
-ancient_power = move('ancient power', 60, True, 10, 100, 'rock', 417, 'special', 'na')
+flamethrower = move('flamethrower', 90, 0, 100, 'fire', 417, 'special', 'na', 0)
+solar_beam = move('solar beam', 120, 100, 100, 'grass', 417, 'special', 'charge', 0)
+air_slash = move('air slash', 75, 30, 95, 'flying', 417, 'special', 'na', 0)
+ancient_power = move('ancient power', 60, 10, 100, 'rock', 417, 'special', 'all+', 1.5)
 
 #Belly drum Charizard moves:
-belly_drum = move('belly drum', 0, True, 100, 100, 'normal', 0, 'status', 'bellydrum')
+belly_drum = move('belly drum', 0, 100, 100, 'normal', 0, 'status', 'bellydrum', 4)
 """I decided to give belly_drum the effect id 'bellydrum' as no other move has
 the same effect as Belly Drum in the original material. Therefore, making a
 generic effect id for this effect is unnecesary. The same is true for other
 moves with effect IDs that resemble their names.
 """
-flare_blitz = move('flare blitz', 120, True, 100, 100, 'fire', 417, 'physical', 'recoil33')
-acrobatics = move('acrobatics', 110, False, 0, 100, 'flying', 417, "physical", 'na')
-metal_claw = move('metal claw', 50, True, 10, 95, 'steel', 417, 'physical', 'atk+1')
+flare_blitz = move('flare blitz', 120, 100, 100, 'fire', 417, 'physical', 'recoil', 0.33)
+acrobatics = move('acrobatics', 110, 0, 100, 'flying', 417, "physical", 'na', 0)
+metal_claw = move('metal claw', 50, 10, 95, 'steel', 417, 'physical', 'atk+', 1.5)
 
 #Triple Arrows Decidueye moves
-triple_arrows = move('triple arrows', 90, True, 50, 100, 'fighting', 1250, 'physical', 'opp_def-1')
-leaf_blade = move('leaf blade', 90, False, 0, 100, 'grass', 1250, 'physical', 'na')
-brave_bird = move('brave bird', 120, True, 100, 100, 'flying', 417, 'physical', 'recoil33')
-steel_wing = move('steel wing', 70, True, 10, 90, 'steel', 417, 'physical', 'def+1')
+triple_arrows = move('triple arrows', 90, 50, 100, 'fighting', 1250, 'physical', 'opp_def-', 0.66)
+leaf_blade = move('leaf blade', 90, 0, 100, 'grass', 1250, 'physical', 'na', 0)
+brave_bird = move('brave bird', 120, 100, 100, 'flying', 417, 'physical', 'recoil', 33)
+steel_wing = move('steel wing', 70, 10, 90, 'steel', 417, 'physical', 'def+', 1.5)
 
 #Swords Dance Decidueye moves
-swords_dance = move('swords dance', 0, True, 100, 100, 'normal', 0, 'status', 'atk+2')
-sucker_punch = move('sucker punch', 70, True, 50, 100, 'dark', 417, 'physical', 'spd+1')
-trailblaze = move('trailblaze', 50, True, 100, 100, 'grass', 417, 'physical', 'spd+1')
-close_combat = move('close combat', 120, True, 100, 100, 'fighting', 417, 'physical', 'defs-1')
+swords_dance = move('swords dance', 0, 100, 100, 'normal', 0, 'status', 'atk+', 2)
+sucker_punch = move('sucker punch', 70, 50, 100, 'dark', 417, 'physical', 'spd+', 1.5)
+trailblaze = move('trailblaze', 50, 100, 100, 'grass', 417, 'physical', 'spd+', 1.5)
+close_combat = move('close combat', 120, 100, 100, 'fighting', 417, 'physical', 'defs-', 0.66)
 
 #Curse Swampert moves
-curse = move('curse', 0, True, 100, 100, 'ghost', 0, 'status', 'curs')
-water_pledge = move('water pledge', 80, False, 0, 100, 'water', 417, 'special', 'na')
-ice_punch = move('ice punch', 75, False, 0, 100, 'ice', 417, 'physical', 'na')
-earthquake = move('earthquake', 100, False, 0, 100, 'ground', 417, 'physical', 'na')
+curse = move('curse', 0, 100, 100, 'ghost', 0, 'status', 'curs', 0)
+water_pledge = move('water pledge', 80, 0, 100, 'water', 417, 'special', 'na', 0)
+ice_punch = move('ice punch', 75, 0, 100, 'ice', 417, 'physical', 'na', 0)
+earthquake = move('earthquake', 100, 0, 100, 'ground', 417, 'physical', 'na', 0)
 
 #Defensive Swampert moves
-ice_beam = move('ice beam', 90, False, 0, 100, 'ice', 417, 'special', 'na')
-surf = move('surf', 90, False, 0, 100, 'water', 417, 'special', 'na')
-life_dew = move('life dew', 0, True, 100, 100, 'water', 0, 'status', 'heal50')
-amnesia = move('amnesia', 0, True, 100, 100, 'psychic', 0, 'status', 'spdef+2')
+ice_beam = move('ice beam', 90, 0, 100, 'ice', 417, 'special', 'na', 0)
+surf = move('surf', 90, 0, 100, 'water', 417, 'special', 'na', 0)
+life_dew = move('life dew', 0, 100, 100, 'water', 0, 'status', 'heal', 2)
+amnesia = move('amnesia', 0, 100, 100, 'psychic', 0, 'status', 'spdef+', 2)
 
 #Toolbox Togekiss moves
-water_pulse = move('water pulse', 60, False, 0, 100, 'water', 417, 'special', 'na')
-shock_wave = move('shock wave', 60, False, 0, 100, 'electric', 417, 'special', 'na')
-gust = move('gust', 40, False, 0, 100, 'flying', 417, 'special', 'na')
+water_pulse = move('water pulse', 60, 0, 100, 'water', 417, 'special', 'na', 0)
+shock_wave = move('shock wave', 60, 0, 100, 'electric', 417, 'special', 'na', 0)
+gust = move('gust', 40, 0, 100, 'flying', 417, 'special', 'na', 0)
 
 #Tank Togekiss moves
-charm = move('charm', 0, True, 100, 100, 'fairy', 417, 'status', 'opp_atk-2')
-shadow_ball = move('shadow ball', 80, True, 20, 100, 'ghost', 417, 'special', 'na')
-magical_leaf = move('magical leaf', 60, False, 0, 100, 'grass', 417, 'special', 'na')
+charm = move('charm', 0, 100, 100, 'fairy', 417, 'status', 'opp_atk-', 0.5)
+shadow_ball = move('shadow ball', 80, 20, 100, 'ghost', 417, 'special', 'na', 0)
+magical_leaf = move('magical leaf', 60, 0, 100, 'grass', 417, 'special', 'na', 0)
 
 """Lists of the movesets: This will be displayed when the player has to choose their move"""
 #Charizard's movesets
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 # imports
 from Pokédex import *
-from Main import dramatic_effect
-# Lists, dictionaries and variables
+import sys
+import time
+#functions
+
+
+def dramatic_effect(txt):
+    """Type characters of a given string one by one.
+
+    It uses a loop and functions imported from sys to print characters
+    in a given string, one by one, while also adding spacing at the end.
+    """
+    for letter in txt:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.04)
+    print('\n')
 
 
 def move_effect(attacker, move, target):
@@ -16,15 +30,23 @@ def move_effect(attacker, move, target):
         return "Error: move selected does not have the attribute 'effect_id'"
     elif move.effect_id == 'na':
         exit
-    elif move.effect_id == 'recoil33':
-        attacker.hp = round(attacker.hp * 0.33)
-        dramatic_effect(f"{attacker} has received recoil damage! ❤️‍🩹")
-        dramatic_effect(f'{attacker} now has 💚{attacker.hp} hp left.')
+    elif move.effect_id == 'recoil':
+        attacker.hp = round(attacker.hp - attacker.hp * move.effect_qty)
+        dramatic_effect(f"{attacker.name} has received recoil damage! ❤️‍🩹")
+        dramatic_effect(f'{attacker.name} now has 💚{attacker.hp} hp left.')
         exit
     elif move.effect_id == 'bellydrum':
         attacker.hp = round(attacker.hp * 0.5)
-        attacker.atk = round(attacker.atk * 4)
+        attacker.atk = round(attacker.atk * move.effect_qtt)
         dramatic_effect(f"{attacker} lost HALF of its hp!")
         dramatic_effect(f"{attacker}'s attack raised to its maximum ✊🆙")
+    elif move.effect_id == 'all+':
+        #use random here
+        attacker.atk = round(attacker.atk * move.effect_qty)
+        attacker.dif = round(attacker.dif * move.effect_qty)
+        attacker.spd = round(attacker.spd * move.effect_qty)
+        attacker.spatk = round(attacker.spatk * move.effect_qty)
+        attacker.spdif = round(attacker.spdif * move.effect_qty)
+        dramatic_effect(f"All of {attacker.name}'s stats increased")
 #Main
 move_effect(charizard1, flare_blitz, decidueye1)
