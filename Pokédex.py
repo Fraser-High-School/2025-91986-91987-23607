ea1629137022548a7c4df1507445c8061a21de8b
23607 <23607@fraser.school.nz>
2025-07-08 18:50:30 +1200

    Finished move_effect
    added an effect for every move ID. Further testing and trialing might be required

--- Pokédex.py
+++ Pokédex.py
@@ -10,7 +10,8 @@ class Pokémon:
     material were disregaded as this adaptation will not use those attributes,
     or those attributes will be imposible to change, like abilities, nature, etc.
     """
-    def __init__(self, name, atk, dif, spd, spatk, spdif, hp, typing, moves = ['placeholder']):
+    def __init__(self, name, atk, dif, spd, spatk,
+                  spdif, hp, typing, moves = ['placeholder']):
         self.name = name
         self.atk = atk
         self.dif = dif
@@ -20,6 +21,23 @@ class Pokémon:
         self.hp = hp
         self.typing = typing
         self.moves = moves
+
+
+    def original_stats(self):
+        """Create a copy of the Pokémon's stats to use as a reference for moves.
+        
+        These copies will help to code and apply the effects of certain moves
+        correctly, like Life Dew or Belly Drum. Life Dew will not restore hp
+        beyond the user's full hp, and Belly Drum will always damage the user by
+        half of its original hp. This functions is meant to be used before the
+        Pokémon battle starts to garantee its usability.
+        """
+        self.og_hp = self.hp
+        self.og_atk = self.atk
+        self.og_dif = self.dif
+        self.og_spd = self.spd
+        self.og_spatk = self.spatk
+        self.og_spdif = self.spdif
 
 
 """I decided to code each Pokémon twice with 2 different names,
@@ -51,7 +69,8 @@ class move:
     Other attributes present in the orignal material were disregarded as they
     will not be used for this simulation, like priority, field effects, etc.
     """
-    def __init__(self, move_name, base_power, effect_chance, acc, ptype, crit_rate, category, effect_id, effect_qty):
+    def __init__(self, move_name, base_power, effect_chance, acc, ptype,
+                  crit_rate, category, effect_id, effect_qty):
         self.move_name = move_name
         self.base_power = base_power
         self.acc = acc
@@ -67,9 +86,9 @@ class move:
 
 #Toolbox Charizard moves:    
 flamethrower = move('flamethrower', 90, 0, 100, 'fire', 417, 'special', 'na', 0)
-solar_beam = move('solar beam', 120, 100, 100, 'grass', 417, 'special', 'charge', 0)
+overgrow = move('overgrow', 100, 100, 100, 'grass', 417, 'special', 'power', 0.5)
 air_slash = move('air slash', 75, 30, 95, 'flying', 417, 'special', 'na', 0)
-ancient_power = move('ancient power', 60, 10, 100, 'rock', 417, 'special', 'all+', 1.5)
+ancient_power = move('ancient power', 60, 10, 100, 'rock', 417, 'special', 'all', 1.5)
 
 #Belly drum Charizard moves:
 belly_drum = move('belly drum', 0, 100, 100, 'normal', 0, 'status', 'bellydrum', 4)
@@ -80,19 +99,19 @@ moves with effect IDs that resemble their names.
 """
 flare_blitz = move('flare blitz', 120, 100, 100, 'fire', 417, 'physical', 'recoil', 0.33)
 acrobatics = move('acrobatics', 110, 0, 100, 'flying', 417, "physical", 'na', 0)
-metal_claw = move('metal claw', 50, 10, 95, 'steel', 417, 'physical', 'atk+', 1.5)
+metal_claw = move('metal claw', 50, 10, 95, 'steel', 417, 'physical', 'atk', 1.5)
 
 #Triple Arrows Decidueye moves
-triple_arrows = move('triple arrows', 90, 50, 100, 'fighting', 1250, 'physical', 'opp_def-', 0.66)
+triple_arrows = move('triple arrows', 90, 50, 100, 'fighting', 1250, 'physical', 'opp_def', 0.66)
 leaf_blade = move('leaf blade', 90, 0, 100, 'grass', 1250, 'physical', 'na', 0)
 brave_bird = move('brave bird', 120, 100, 100, 'flying', 417, 'physical', 'recoil', 33)
-steel_wing = move('steel wing', 70, 10, 90, 'steel', 417, 'physical', 'def+', 1.5)
+steel_wing = move('steel wing', 70, 10, 90, 'steel', 417, 'physical', 'def', 1.5)
 
 #Swords Dance Decidueye moves
-swords_dance = move('swords dance', 0, 100, 100, 'normal', 0, 'status', 'atk+', 2)
-sucker_punch = move('sucker punch', 70, 50, 100, 'dark', 417, 'physical', 'spd+', 1.5)
-trailblaze = move('trailblaze', 50, 100, 100, 'grass', 417, 'physical', 'spd+', 1.5)
-close_combat = move('close combat', 120, 100, 100, 'fighting', 417, 'physical', 'defs-', 0.66)
+swords_dance = move('swords dance', 0, 100, 100, 'normal', 0, 'status', 'atk', 2)
+sucker_punch = move('sucker punch', 70, 50, 100, 'dark', 417, 'physical', 'spd', 1.5)
+trailblaze = move('trailblaze', 50, 100, 100, 'grass', 417, 'physical', 'spd', 1.5)
+close_combat = move('close combat', 120, 100, 100, 'fighting', 417, 'physical', 'defs', 0.66)
 
 #Curse Swampert moves
 curse = move('curse', 0, 100, 100, 'ghost', 0, 'status', 'curs', 0)
@@ -103,8 +122,8 @@ earthquake = move('earthquake', 100, 0, 100, 'ground', 417, 'physical', 'na', 0)
 #Defensive Swampert moves
 ice_beam = move('ice beam', 90, 0, 100, 'ice', 417, 'special', 'na', 0)
 surf = move('surf', 90, 0, 100, 'water', 417, 'special', 'na', 0)
-life_dew = move('life dew', 0, 100, 100, 'water', 0, 'status', 'heal', 2)
-amnesia = move('amnesia', 0, 100, 100, 'psychic', 0, 'status', 'spdef+', 2)
+life_dew = move('life dew', 0, 100, 100, 'water', 0, 'status', 'heal', 0.5)
+amnesia = move('amnesia', 0, 100, 100, 'psychic', 0, 'status', 'spdef', 2)
 
 #Toolbox Togekiss moves
 water_pulse = move('water pulse', 60, 0, 100, 'water', 417, 'special', 'na', 0)
@@ -112,13 +131,13 @@ shock_wave = move('shock wave', 60, 0, 100, 'electric', 417, 'special', 'na', 0)
 gust = move('gust', 40, 0, 100, 'flying', 417, 'special', 'na', 0)
 
 #Tank Togekiss moves
-charm = move('charm', 0, 100, 100, 'fairy', 417, 'status', 'opp_atk-', 0.5)
-shadow_ball = move('shadow ball', 80, 20, 100, 'ghost', 417, 'special', 'na', 0)
+charm = move('charm', 0, 100, 100, 'fairy', 417, 'status', 'opp_atk', 0.5)
+shadow_ball = move('shadow ball', 80, 20, 100, 'ghost', 417, 'special', 'opp_spdef', 0.66)
 magical_leaf = move('magical leaf', 60, 0, 100, 'grass', 417, 'special', 'na', 0)
 
 """Lists of the movesets: This will be displayed when the player has to choose their move"""
 #Charizard's movesets
-toolbox_charizard = [flamethrower, solar_beam, air_slash, ancient_power]
+toolbox_charizard = [flamethrower, overgrow, air_slash, ancient_power]
 bellydrum_charizard = [belly_drum, flare_blitz, acrobatics, metal_claw]
 #Decidueyes movesets
 triplearrows_decidueye = [triple_arrows, leaf_blade, brave_bird, steel_wing]
